8091227688f65d07f960c7ee9006e628a0a66a78
bryanoke25 <bryan.oke25@gmail.com>
2025-10-22 18:55:22 +0100

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,12 @@
+
+tasks = []
+
+print("Welcome, this is your TO-DO list. please choose an option from the following")
+print("1. Wiew your list")
+print("2. Add an item to your list")
+print("3. Delete an item to your list")
+print("4. Exit your list")
+choice = int(input("please put on of the items above : "))
+
+if choice == 1:
+    print(tasks)
